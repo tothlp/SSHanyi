8d55f86ba5f7090bee42d4da3b256a514fd369ed
Tóth László <dev@tothlp.hu>
2023-12-14 22:55:50 +0100

    build: support M-chip Mac architecture

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,10 @@ repositories {
 
 kotlin {
     val hostOs = System.getProperty("os.name")
+    val arch = System.getProperty("os.arch")
     val isMingwX64 = hostOs.startsWith("Windows")
     val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Mac OS X" -> if (arch == "x86_64" ) macosX64("native") else macosArm64("native")
         hostOs == "Linux" -> linuxX64("native")
         isMingwX64 -> mingwX64("native")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
